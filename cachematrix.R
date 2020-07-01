0e331c90e69d09c35ceeea5717ea7ee3858c9f8b
saiteja rapeti <saiteja.rapeti@gmail.com>
2020-07-01 12:19:34 +0530

    new commit

--- cachematrix.R
+++ cachematrix.R
@@ -9,8 +9,11 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
     x <<- y
     s <<- NULL
   }
+  #this is to get the normal matrix 
   get <- function() x
+  #the below function is to solve and get inverse matrix
   setsolve <- function(solve) s <<- solve
+  #to return the inversed matrix
   getsolve <- function() s
   list(set = set, get = get,
        setsolve = setsolve,
@@ -23,6 +26,7 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   s <- x$getsolve()
+  #if the matrix can be inversed then its print
   if(!is.null(s)) {
     message("inversed matrix is getting ready for you")
     return(s)
